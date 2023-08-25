84d723c1732f6d2d9974fdbda3c4458878e729e2
Joel Erni <joel.erni@hispeed.ch>
2023-08-25 14:52:37 +0200

    Maosdmaodmasod

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ var reveals = document.querySelectorAll(".reveal");
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 200;
+        var elementVisible = 150;
 
         if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("show");
